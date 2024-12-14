6cb0ac2954f68ae072d1689754a31995c81bf89c
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-14 14:13:42 -0300

    first use mui

--- components/pages/login.tsx
+++ components/pages/login.tsx
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 export default function Login(){
     return (
         <>
             <h1>LOgiN</h1>
+            <Button variant="contained">OK</Button>
         </>
     )
 }
