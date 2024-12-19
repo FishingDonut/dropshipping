b63aea8f4d80eaac37047443e4a03b4436d2b0f5
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-19 09:34:31 -0300

    update input

--- components/layouts/Input/Input.tsx
+++ components/layouts/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { FormLabel, Input as InputMui, Typography } from "@mui/material";
+import { Input as InputMui, FormControl, FormHelperText, InputLabel } from "@mui/material";
 
 interface InputProps {
     name: string,
@@ -10,18 +10,18 @@ interface InputProps {
 
 export default function Input({name, placeholder = "", label = "", type = "text", mensageError="", ...res }: InputProps) {
 
-    const customInput = {
+  const customInput = {
         color: "white",
         "&::before": { borderBottom: "0 solid rgba(0,0,0,0)" },
         "&:hover": { borderBottom: "2px solid #707070" },
         "&::after": { borderBottom: "2px solid #ffff" },
     }
 
-    return (
-        <>
-            <FormLabel htmlFor={name} sx={{ color: "white" }}>{label}</FormLabel>
-            <InputMui id={name} name={name} sx={customInput} placeholder={placeholder} type={type} {...res} autoComplete="off"></InputMui>
-            <Typography sx={{color: "red"}} variant="subtitle1">{mensageError}</Typography>
-        </>
-    );
+  return (
+    <FormControl error={mensageError ? true : false} variant="standard">
+            <InputLabel htmlFor={name} sx={{ color: "white" }}>{label}</InputLabel>
+            <InputMui id={name} name={name} sx={customInput} placeholder={placeholder} error={mensageError ? true : false } type={type} {...res} autoComplete="off"></InputMui>
+            {mensageError && <FormHelperText id={name + "-error"}>{mensageError}</FormHelperText>}
+    </FormControl>
+  );
 }
