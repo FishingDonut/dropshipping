6f11309daf528847d2f389fd6397f80ce73d4c7e
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-19 10:54:25 -0300

    update input

--- components/layouts/Input/Input.tsx
+++ components/layouts/Input/Input.tsx
@@ -9,17 +9,10 @@ interface InputProps {
 
 export default function Input({name, placeholder = "", label = "", type = "text", mensageError="", ...res }: InputProps) {
 
-  const customInput = {
-        color: "white",
-        "&::before": { borderBottom: "0 solid rgba(0,0,0,0)" },
-        "&:hover": { borderBottom: "2px solid #707070" },
-        "&::after": { borderBottom: "2px solid #ffff" },
-    }
-
   return (
     <FormControl error={mensageError ? true : false} variant="standard">
-            <InputLabel htmlFor={name} sx={{ color: "white" }}>{label}</InputLabel>
-            <InputMui id={name} name={name} sx={customInput} placeholder={placeholder} error={mensageError ? true : false } type={type} {...res} autoComplete="off"></InputMui>
+            <InputLabel htmlFor={name}>{label}</InputLabel>
+            <InputMui id={name} name={name} placeholder={placeholder} error={mensageError ? true : false } type={type} {...res} autoComplete="off"></InputMui>
             {mensageError && <FormHelperText id={name + "-error"}>{mensageError}</FormHelperText>}
     </FormControl>
   );
