2601cd13462c0e7b4da1d83c19a72db1897e6d98
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-20 12:11:35 -0300

    english

--- components/pages/form/registerSchema.ts
+++ components/pages/form/registerSchema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-    fullName: z.string().min(4, "Minimo 4 caracteres."),
-    email: z.string().email("Tem que ser um email."),
-    password: z.string().min(8, "Minimo 8 caracteres."),
-    passwordConfirm: z.string().min(8, "Minimo 8 caracteres.")
+    fullName: z.string().min(4, "Minimum 4 characters."),
+    email: z.string().email("Must be a valid email."),
+    password: z.string().min(8, "Minimum 8 characters."),
+    passwordConfirm: z.string().min(8, "Minimum 8 characters.")
 }).refine((data) => data.password === data.passwordConfirm, {
     path: ['passwordConfirm'],
-    message: "As senhas não coincidem."
+    message: "Passwords do not match."
 });
 
-export type RegisterSchema = z.infer<typeof registerSchema>
+export type RegisterSchema = z.infer<typeof registerSchema>;
