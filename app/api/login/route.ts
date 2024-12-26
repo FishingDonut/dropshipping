df07c1ab1d18d6e85a3b73071fe2a144ff4cc6d3
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-26 16:22:12 -0300

    update api login

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,35 +1,39 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Prisma } from "@prisma/client";
 import { prisma } from "@/services/PristmaClientService";
-import jwt from 'jsonwebtoken';
-
-const JWT_SECRET = '1234';
 
 interface ILoginData {
     email: string,
     password: string
 };
 
-export async function loginApi(request: NextRequest){
+export async function POST(request: NextRequest){
     const body: ILoginData = await request.json()
 
     const { email, password } = body;
-
+    
     const loginUser: Prisma.UsersCreateInput | null = await prisma.users.findUnique({
         where: {
             email: email
         }
     });
 
-    if(!loginUser){
-        return new Response("User not found", { status: 400 });
+    if (!loginUser) {
+        return new Response(JSON.stringify({ error: "User not found" }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' }
+        });
     }
-
-    if(loginUser.password != password){
-        return new Response("Wrong password", { status: 400 })
+    
+    if (loginUser.password !== password) {
+        return new Response(JSON.stringify({ error: "Wrong password" }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' }
+        });
     }
-
-    const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' });
-
-    return new Response(token, { status: 200, headers: { 'Content-Type': 'application/json' }});
+    
+    return new Response(JSON.stringify({ message: loginUser}), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+    });    
 } 
