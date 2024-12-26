1ba537f72c790c27354936d0f15d0f4da4e415ce
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-26 16:22:46 -0300

    update provider login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,44 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+
+        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/login/`, {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password
+          })
+        });
+
+        const body = await response.json();
+        console.log(response.status);
+        console.log(body);
+
+        return null;
+
+      }
     })
-  ]
+  ],
+  pages: {
+    signIn: "/login",
+  }
 };
 
-export const GET = NextAuth(authOptions); // Exporta o método GET
-export const POST = NextAuth(authOptions); // Exporta o método POST 
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
