361542a133f7bdafa5ad6adaa6fb95b69f2f6846
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-28 00:24:29 -0300

    --save 00:24

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
 import NextAuth, { NextAuthOptions  } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 declare module "next-auth" {
@@ -84,5 +83,4 @@ export const authOptions: NextAuthOptions = {
   }
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
