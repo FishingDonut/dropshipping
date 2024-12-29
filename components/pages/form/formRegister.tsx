9f94ed068d3edbfc5dbd33ba5066ce6b8031fe46
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-12-29 12:38:07 -0300

    update route create user to register

--- components/pages/form/formRegister.tsx
+++ components/pages/form/formRegister.tsx
@@ -14,7 +14,7 @@ export default function FormRegister() {
 
     const handleRegisterFormSuccess = async (data: RegisterSchema) => {
         try {
-            const response = await fetch("/api/createUser", {
+            const response = await fetch("/api/register", {
                 method: "POST",
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(data)
