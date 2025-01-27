5a31f5165d3a9601750352aa94020ecd6f2db92e
FishingDonut <protronsdimi@gmail.com>
2025-01-27 16:51:54 -0300

    update view product

--- components/pages/product/mainProduct.tsx
+++ components/pages/product/mainProduct.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { Box, Card, CardContent, CardMedia, Paper, styled, Typography } from "@mui/material";
+import { Box, Chip, Divider, IconButton, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Grid from '@mui/material/Grid2';
-import Link from "next/link";
+import { ArrowBack, Share, FavoriteBorder, LocalActivity } from "@mui/icons-material";
 
 const MainProduct = () => {
     const [product, setProduct] = useState<any>(null);
@@ -38,47 +38,59 @@ const MainProduct = () => {
     };
 
     return (
-        <Grid alignItems="center" sx={customBoxMain} container size={12}>
+        <Grid spacing={1} padding={2} alignItems="center" sx={customBoxMain} container size={12}>
             <Grid alignItems="center" container size={12}>
 
                 <Grid container size={12}>
                     <Grid alignItems="left" size={4}>
-                        <Typography align="left">BACK</Typography>
+                        <IconButton>
+                            <ArrowBack />
+                        </IconButton>
                     </Grid>
-                    <Grid alignItems="center" size={4}>
-                        <Typography align="center">NAME</Typography>
+                    <Grid display="flex" justifyContent="center" alignItems="center" size={4}>
+                        <Typography fontWeight="bold" align="center">NAME</Typography>
                     </Grid>
-                    <Grid alignItems="right" size={4}>
-                        <Typography align="right">SHARE</Typography>
+                    <Grid textAlign="right" size={4}>
+                        <IconButton>
+                            <Share />
+                        </IconButton>
                     </Grid>
                 </Grid>
 
                 <Grid size={12}>
-                    <img width="5000px" src="https://http2.mlstatic.com/D_NQ_NP_734313-MLA52042255660_102022-O.webp" />
+                    <Box component="img" width="100%" borderRadius={2} src="https://http2.mlstatic.com/D_NQ_NP_734313-MLA52042255660_102022-O.webp" />
                 </Grid>
 
                 <Grid container size={12}>
-                    <Grid container size={10}>
-                        <Typography align="left">FULLNAME</Typography>
-                    </Grid>
-                    <Grid container size={2}>
-                        <Typography align="right">SHARE</Typography>
-                    </Grid>
-                    <Grid container size={6}>
-                        <Typography align="right">Desconto</Typography>
-                    </Grid>
-                    <Grid container size={6}>
-                        <Typography align="right">review</Typography>
+                    <Grid container size={12}>
+                        <Grid spacing={0} container display="flex" justifyContent="left" alignItems="center" size={10}>
+                            <Grid>
+                                <Chip label="23.4%"></Chip>
+                            </Grid>
+                            <Grid>
+                                <IconButton><LocalActivity color="primary" /></IconButton>
+                            </Grid>
+                            <Grid>
+                                <Typography variant="body2" color="text.secondary">4.5 ( 3242 Reviews )</Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container size={2}>
+                            <IconButton>
+                                <FavoriteBorder />
+                            </IconButton>
+                        </Grid>
                     </Grid>
                 </Grid>
 
                 <Grid container size={12}>
                     <Grid size={12}>
-                        <Typography variant="h6" align="left">Description</Typography>
-                        <Typography variant="subtitle2" align="left">Eu sou uma descrição gerada por Inteligência Artificial, criada para fornecer exemplos, explicações ou respostas de forma clara e objetiva. Meu objetivo é ajudar você com informações precisas e úteis em diversas áreas, desde programação até temas gerais.</Typography>
+                        <Typography variant="h6" fontWeight="bold" align="left">Description</Typography>
+                        <Typography variant="body2" color="text.secondary" align="left">Eu sou uma descrição gerada por Inteligência Artificial, criada para fornecer exemplos, explicações ou respostas de forma clara e objetiva. Meu objetivo é ajudar você com informações precisas e úteis em diversas áreas, desde programação até temas gerais.</Typography>
                     </Grid>
                 </Grid>
 
+                <Divider sx={{ marginY: 2}}/>
+                
                 <Grid container size={12}>
                     CATEGORIAS
                 </Grid >
