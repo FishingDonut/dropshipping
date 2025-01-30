8d3cfb5d189d65cfcfdf8d4338db02e6b2eea11a
FishingDonut <protronsdimi@gmail.com>
2025-01-30 09:51:54 -0300

    params id in component

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,7 +3,7 @@ import MainProduct from '@/components/pages/product/mainProduct';
 const pages = async({ params }: { params: Promise<{ id: number }> }) => { 
     const id = (await params).id;
     return (
-        <MainProduct/>
+        <MainProduct id={id}/>
     );
 }
 
